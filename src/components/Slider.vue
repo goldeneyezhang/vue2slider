30ede8bdd484928b7d0e4908488fbebd977257cc
goldeneyezhang <goldeneyezhang@163.com>
2017-11-13 09:55:32 +0800

    vue2 slider

---
+++ src/components/Slider.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="section">
+        <div>
+            <!--热门推荐-->
+            <div class="swiper-container" ref="slider">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="./fixture/sliders/t1.svg"/>
+                    </div>
+                </div>
+                 <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="./fixture/sliders/t2.svg"/>
+                    </div>
+                </div>
+                <div class="swiper-pagination" ref="pagination">
+            </div>
+            <!--快讯-->
+        </div>
+        <div class="section">
+            <!--新书上架-->
+        </div>
+        <div class="section">
+            <!--编辑推荐-->
+        </div>
+    </div>
+</template>
+<script>
+    import Swiper from 'swiper'
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Main from './Main.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
