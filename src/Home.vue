a9c309021825a762505d8fe4dad68170cc42f02c
goldeneyezhang <goldeneyezhang@163.com>
2017-11-13 10:57:51 +0800

    vue2 slider

---
+++ src/Home.vue
@@ -0,0 +1,22 @@
+<div class="section">
+    <book-list :books="lastUpdated" heading="最新更新">
+    </book-list>
+</div>
+<div class="section">
+    <book-list :books="recommended" heading="编辑推荐">
+    </book-list>
+</div>
+<script>
+    import BookList from './components/BookList.vue'
+    export default{
+        data(){
+            announcement:'今日上架的图书全部8折',
+            slides:[
+                    {id:1,img_url:'./fixtures/sliders/t1.svg'},
+                    {id:2,img_url:'./fixtures/sliders/t2.svg'}
+                ],
+            latestUpdated:[],
+            recommended:[]
+        }
+    }
+</script>
